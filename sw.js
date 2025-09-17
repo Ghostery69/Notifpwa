dc945c87acaa6bc7fe7d346dbbe796fe4add8fb8
Ghostery69 <157644164+Ghostery69@users.noreply.github.com>
2025-09-17 03:13:00 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 self.addEventListener('install', event => {
-  console.log('Service Worker installé');
+  console.log('Service Worker installé ✅');
 });
 
 self.addEventListener('activate', event => {
-  console.log('Service Worker activé');
+  console.log('Service Worker activé ✅');
 });
 
 self.addEventListener('fetch', event => {
